e003a80caae5590ce64960d384f6f6612345dbbe
Blade Boles <bladerboles@gmail.com>
2019-12-04 18:38:20 -0700

    first commit with jQuery.  Having an issue with the check button.

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+console.log('test1');
+
+//function for adding new items to the list
+
+$('form').submit(function(event) {
+  event.preventDefault();
+
+  console.log('test2');
+
+  let newItem = $('input').val();
+
+  console.log(newItem);
+
+  let newEntry = $(`<li>
+  <span class="shopping-item">${newItem}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+  </li>`);
+
+  $('.shopping-list').append(newEntry);
+  
+  console.log(`added ${newItem}`);
+
+});
+
+//function for the delete button
+
+$('div').on('click', 'button.shopping-item-delete', function(event) {
+  event.preventDefault();
+  console.log('clicked a delete button');
+  $(this).closest('li').remove(); 
+  
+  
+});
+
+//function for the check button
+
+$('div').on('click', 'button.shopping-item-toggle', function(event) {
+  console.log('clicked a toggle button');
+  event.preventDefault();
+  $(this).closest('li').children('span').toggleClass('shopping-item__checked');  
+});
+
+
